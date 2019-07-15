83120b3b9888984794bf885d8d70412262bce4ee
Ying Huang <huangying270@gmail.com>
2019-07-15 00:35:15 -0400

    added html content and pseudocoded in JS

---
+++ assets/javascript/trainschedule.js
@@ -0,0 +1,16 @@
+$(document).ready(function(){
+
+
+
+
+
+})
+
+/*
+
+initilize firebase
+variables for html IDs for new trains
+push new input to variables each time
+use child_added and snapshot to add new rows to table
+
+*/
