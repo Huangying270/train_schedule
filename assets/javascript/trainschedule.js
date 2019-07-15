a5bb9a2e4e6ad36675ad8c9ccd4779cf8d7da1ee
Ying Huang <huangying270@gmail.com>
2019-07-15 18:44:28 -0400

    added moment.js and initialized firebase to collect inputs

--- assets/javascript/trainschedule.js
+++ assets/javascript/trainschedule.js
@@ -1,6 +1,46 @@
-$(document).ready(function(){
+$(document).ready(function () {
 
+    var config = {
+        apiKey: "AIzaSyCUYK26JNWYfLWmDphAacrOscRkHMrpW18",
+        authDomain: "bootcamp-work.firebaseapp.com",
+        databaseURL: "https://bootcamp-work.firebaseio.com",
+        projectId: "bootcamp-work",
+        storageBucket: "bootcamp-work.appspot.com",
+        messagingSenderId: "922464549797",
+        appId: "1:922464549797:web:2dce58ab7f360ef9"
+    };
 
+    firebase.initilizeApp(config);
+
+    var database = firebase.database();
+
+    $("#addTrain").on("click", function(event){
+
+        event.preventDefault();
+
+        var trainName = "";
+        var destination = "";
+        var firstTrain = "";
+        var interval = "";
+
+        trainName = $("#trainName").val().trim();
+        destination = $("#destination").val().trim();
+        firstTrain = $("#firstTrain").val().trim();
+        interval = $("#interval").val().trim();
+
+        if (trainName !== "" && destination !== "" && firstTrain !== "" && interval !== "") {
+
+        database.ref().push({
+            trainName: trainName,
+            destination: destination,
+            firstTrain: firstTrain,
+            interval: interval,
+        });
+    } else {
+        alert("PLEASE COMPLETE ALL FIELDS");
+    }
+
+    });
 
 
 
